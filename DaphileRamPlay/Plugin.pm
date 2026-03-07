d104c3ae62e6c6ccd5ce04dcc4fffd430fc43dc0
EmilioRusso77 <98224961+EmilioRusso77@users.noreply.github.com>
2026-03-07 16:53:02 +0100

    Add debug logging for playlist events
    Added debug subscription to capture all playlist events.

--- DaphileRamPlay/Plugin.pm
+++ DaphileRamPlay/Plugin.pm
@@ -50,7 +50,17 @@ sub initPlugin {
         \&onNewSong,
         [['playlist'], ['newsong']]
     );
-
+	
+	# DEBUG - cattura tutti gli eventi playlist
+    Slim::Control::Request::subscribe(
+        sub {
+            my $request = shift;
+            my $client  = $request->client() || return;
+            $log->info("EVENT: " . $request->getRequestString());
+        },
+        [['playlist']]
+    );
+	
     $log->info("DaphileRamPlay plugin initialized (v1.4 - early trigger)");
 
     $class->SUPER::initPlugin(@_);
